1b348fbf47b91ef98c24f7108467f8f4e608f35a
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-09-11 13:23:43 -0300

    Add Crud actions to entities

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,6 @@
 class Group < ApplicationRecord
   belongs_to :user
+  has_many :entities
 
   validates :name, presence: true, length: { maximum: 100 }
   validates :name, presence: true, length: { maximum: 200 }
